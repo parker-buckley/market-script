481778db24791620f19418cbd2ef4f5c2d0f7ef6
Parker Buckley <parker.brett.buckley@gmail.com>
2024-10-29 10:48:01 -0500

    Support local dev

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: '/market-script',
-  assetPrefix: '/market-script/',
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/market-script/' : '',
+  basePath: isProd ? '/market-script' : '',
+  output: 'export'
 };
 
 export default nextConfig;
